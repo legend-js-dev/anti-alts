b102f46ed78be87d540892891c15e24f62e4325f
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2021-05-11 18:00:10 +0545

    xd

---
+++ commands/info/help.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'help',
+	run: async (client, message, args) => {
+		const embed = new Discord.MessageEmbed()
+			.setTitle(client.user.username + ` | Help`)
+			.setThumbnail(client.user.displayAvatarURL())
+			.addField(`Anti Alt`, '`config-alt` | `bypass-alt` | `fetch-alts`')
+			.setDescription(`**The commands are listed below**`)
+			.setColor(`GREEN`)
+			.setFooter(message.guild.name + " | Made by ant#0768 & LΣGΣПD#0001", message.guild.iconURL({ dynamic: true }));
+		return message.channel.send({ embed: embed });
+	}
+};
